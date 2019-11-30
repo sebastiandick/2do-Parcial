7476034018e153891dab7d370b656d272fc7a665
gustavodaza2013@gmail.com <visual_community@outlook.com>
2019-11-30 12:45:37 -0400

    Modificamos los errores del programa, realizamos la prueba, corregimos y finalizamos

---
+++ Dick/Dick/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dick
+{
+    public class Company
+    {
+
+        private string _name; private double _saveproject;
+
+        public string Name { get { return _name; } }
+        public double SaveProject { get { return _saveproject; } }
+
+        public Company(string name, double saveproject) { _name = name; _saveproject = saveproject; }
+
+        public void Save(double save) { _saveproject += save; }
+
+       public void Spend(double save) { _saveproject -= save; }
+       public void ChangeName(string name) { _name = name; }
+
+    }
+
+}
---
+++ Dick/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Dick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaDelSave()
+        {
+            //Organizar
+            Company comp = new Company("hola", 35.35);
+            double ValorEsp = 45.35;
+            //Actuar
+            comp.Save(10);
+            double ValorOb = comp.SaveProject;
+            //Afirmar
+            Assert.AreEqual(ValorEsp, ValorOb);
+        }
+
+        [TestMethod]
+        public void PruebaDelSpend()
+        {
+            //Organizar
+            Company comp = new Company("hola", 35.35);
+            double ValorEsp = 25.35;
+            //Actuar
+            comp.Spend(10);
+            double ValorOb = comp.SaveProject;
+            //Afirmar
+            Assert.AreEqual(ValorEsp, ValorOb);
+
+        }
+
+        [TestMethod]
+        public void PruebaDelChangeName()
+        {
+            //Organizar
+            Company comp = new Company("hola", 35.35);
+            string ValorEsp = "Sebas";
+            //Actuar
+            comp.ChangeName("Sebas");
+            string ValorOb = comp.Name;
+            //Afirmar
+            Assert.AreEqual(ValorEsp, ValorOb);
+        }
+    }
+}
